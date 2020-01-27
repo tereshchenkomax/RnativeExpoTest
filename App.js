35f7fe2d364c41af2bd9c54f5c21819014d6b316
tereshchenkomax <tereshchenkomax@gmail.com>
2020-01-27 17:27:29 +0200

    user list with hooks v0

--- App.js
+++ App.js
@@ -1,34 +1,90 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
+import {SearchBar, ListItem} from 'react-native-elements';
 import axios from 'axios';
 import ErrorBoundary from "./components/ErrorBoundary";
-
-const fetchData = async () => {
-    try {
-        let users = await axios.get('https://jsonplaceholder.typicode.com/users'); //TODO new URL
-        return await users.data;
-    } catch (error) {
-        throw error;
-    }
-};
 
 export default function App() {
 
     const [data, setData] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, isLoading] = useState(false);
 
+    const searchFilterFunction = text => {
+        const arrayholder = [];
+        const newData = arrayholder.filter(item => {
+            const itemData = `${item.name.toUpperCase()}`;
+            const textData = text.toUpperCase();
 
-    useEffect( () => {
+            return itemData.indexOf(textData) > -1;
+        });
+
+        setData(data)
+    };
+
+    const fetchData = async () => {
+        try {
+            let users = await axios.get('https://jsonplaceholder.typicode.com/users'); //TODO new URL
+            return await users.data;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const renderHeader = () => {
+        return (
+            <SearchBar
+                placeholder="Type Here..."
+                lightTheme
+                round
+                onChangeText={text => searchFilterFunction(text)}
+                autoCorrect={false}
+            />
+        );
+    };
+
+    useEffect(() => {
+        isLoading(true);
         fetchData()
-            .then(data => setData(data))
+            .then(data => {
+                setData(data);
+                isLoading(false);
+            })
+            .catch(err => setError(err))
+
     }, []);
 
+    if (loading) {
+        return (
+            <ErrorBoundary>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <ActivityIndicator />
+                </View>
+            </ErrorBoundary>
+        )
+    }
     return (
         <ErrorBoundary>
-            <View style={styles.container}>
+            <View style={styles.filter}>
                 <Text>Open up App.js to start working on your app!</Text>
                 {data.map(user => {
-                  return <Text key={user.id}>{user.name}</Text>
+                    return (
+                        <View key={user.id}><Text>{user.name}</Text>
+                            <Text>{user.email}</Text></View>
+                    )
                 })}
+            </View>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <FlatList
+                    data={data}
+                    renderItem={({item}) => (
+                        <ListItem
+                            title={item.name}
+                        />
+                    )}
+                    keyExtractor={item => item.id}
+                />
             </View>
         </ErrorBoundary>
     );
@@ -36,101 +92,127 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 10,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    filter: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
-// import React, {useState} from 'react';
+
+// import React, { Component } from 'react';
+// import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+// import { ListItem, SearchBar } from 'react-native-elements';
 //
-// import {
-//     StyleSheet,
-//     Text,
-//     View,
-//     TouchableOpacity,
-//     TextInput,
-//     ScrollView
-// } from 'react-native';
+// class FlatListDemo extends Component {
+//     constructor(props) {
+//         super(props);
 //
-// import ErrorBoundary from 'react-native-error-boundary'
-// // import ErrorBoundary from './components/ErrorBoundary'
-// import TodoList from './components/TodoList';
-// import Icon from 'react-native-vector-icons/Feather';
+//         this.state = {
+//             loading: false,
+//             data: [],
+//             error: null,
+//         };
 //
-// const errorHandler = (error, stackTrace) => {
-//     /* Log the error to an error reporting service */
-//     console.log(error, stackTrace)
-// }
+//         this.arrayholder = [];
+//     }
 //
-// export default function App() {
-//     const [value, setValue] = useState('');
-//     const [todos, setTodos] = useState([]);
+//     componentDidMount() {
+//         this.makeRemoteRequest();
+//     }
 //
-//     const addTodo = () => {
-//         if (value.length > 0) {
-//             setTodos([...todos, {text: value, key: Date.now(), checked: false}]);
-//             setValue('');
-//         }
+//     makeRemoteRequest = () => {
+//         const url = `https://randomuser.me/api/?&results=20`;
+//         this.setState({ loading: true });
+//
+//         fetch(url)
+//             .then(res => res.json())
+//             .then(res => {
+//                 this.setState({
+//                     data: res.results,
+//                     error: res.error || null,
+//                     loading: false,
+//                 });
+//                 this.arrayholder = res.results;
+//             })
+//             .catch(error => {
+//                 this.setState({ error, loading: false });
+//             });
 //     };
 //
+//     renderSeparator = () => {
+//         return (
+//             <View
+//                 style={{
+//                     height: 1,
+//                     width: '86%',
+//                     backgroundColor: '#CED0CE',
+//                     marginLeft: '14%',
+//                 }}
+//             />
+//         );
+//     };
 //
-//     return (
-//         <ErrorBoundary onError={errorHandler}>
-//             <View style={styles.container}>
-//                 <Text style={styles.header}>Todo List</Text>
-//                 <View style={styles.textInputContainer}>
-//                     <TextInput
-//                         style={styles.textInput}
-//                         multiline={true}
-//                         placeholder="What do you want to do today?"
-//                         placeholderTextColor="#abbabb"
-//                         value={value}
-//                         onChangeText={value => setValue(value)}
-//                     />
-//                     <Text onPress={() => addTodo()}>
-//                         >
-//                         <Icon name="plus" size={30} color="blue" style={{marginLeft: 15}}/>
-//                     </Text>
+//     searchFilterFunction = text => {
+//         this.setState({
+//             value: text,
+//         });
+//
+//         const newData = this.arrayholder.filter(item => {
+//             const itemData = `${item.name.title.toUpperCase()} ${item.name.first.toUpperCase()} ${item.name.last.toUpperCase()}`;
+//             const textData = text.toUpperCase();
+//
+//             return itemData.indexOf(textData) > -1;
+//         });
+//         this.setState({
+//             data: newData,
+//         });
+//     };
+//
+//     renderHeader = () => {
+//         return (
+//             <SearchBar
+//                 placeholder="Type Here..."
+//                 lightTheme
+//                 round
+//                 onChangeText={text => this.searchFilterFunction(text)}
+//                 autoCorrect={false}
+//                 value={this.state.value}
+//             />
+//         );
+//     };
+//
+//     render() {
+//         if (this.state.loading) {
+//             return (
+//                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//                     <ActivityIndicator />
 //                 </View>
-//                 <ScrollView style={{width: '100%'}}>
-//                     {todos.map(item => (
-//                         <TodoList text={item.text} key={item.key}/>
-//                     ))}
-//                 </ScrollView>
+//             );
+//         }
+//         return (
+//             <View style={{ flex: 1 }}>
+//                 <FlatList
+//                     data={this.state.data}
+//                     renderItem={({ item }) => (
+//                         <ListItem
+//                             leftAvatar={{ source: { uri: item.picture.thumbnail } }}
+//                             title={`${item.name.first} ${item.name.last}`}
+//                             subtitle={item.email}
+//                         />
+//                     )}
+//                     keyExtractor={item => item.email}
+//                     ItemSeparatorComponent={this.renderSeparator}
+//                     ListHeaderComponent={this.renderHeader}
+//                 />
 //             </View>
-//         </ErrorBoundary>
-//     );
+//         );
+//     }
 // }
 //
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         justifyContent: 'flex-start',
-//         alignItems: 'center',
-//         backgroundColor: '#F5FCFF'
-//     },
-//     header: {
-//         marginTop: '15%',
-//         fontSize: 20,
-//         color: 'red',
-//         paddingBottom: 10
-//     },
-//     textInputContainer: {
-//         flexDirection: 'row',
-//         alignItems: 'baseline',
-//         borderColor: 'black',
-//         borderBottomWidth: 1,
-//         paddingRight: 10,
-//         paddingBottom: 10
-//     },
-//     textInput: {
-//         flex: 1,
-//         height: 20,
-//         fontSize: 18,
-//         fontWeight: 'bold',
-//         color: 'black',
-//         paddingLeft: 10,
-//         minHeight: '3%'
-//     }
-// });
+// export default FlatListDemo;
